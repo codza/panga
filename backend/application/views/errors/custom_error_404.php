7717d0cc1affe0934fb0ca4e992b6e443ef3f7a9
codza <n.lodza@yahoo.fr>
2016-01-11 01:28:14 +0100

    adding jquery and the custome error_404 page

---
+++ backend/application/views/errors/custom_error_404.php
@@ -0,0 +1,18 @@
+<div id="rhu_error" class="container" xmlns="http://www.w3.org/1999/html">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <span class="error-number">404</span>
+                <span class="error-message">Page Not Found</span>
+                <span class="error-details">
+                    Oops! The Page you requested was not found!
+                </span>
+                <div class="error-actions">
+                    <a href="<?php echo base_url();?>" class="btn btn-primary">
+                        Go Home
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
