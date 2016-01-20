4736aec4f910787ecb6bf5e2596eedaae6ac06e9
codza <n.lodza@yahoo.fr>
2016-01-20 10:39:29 +0100

    adding the developement environment

--- www/index.php
+++ www/index.php
@@ -6,6 +6,9 @@ $pangaDir="../backend/";
 		case 'C:\wamp\www\02_BASE\panga\www':
 			define('ENVIRONMENT', 'development');
 		break;
+                case '/home/lodza/phpwebsite/panga/www':
+			define('ENVIRONMENT', 'development');
+		break;
 		default:
 			define('ENVIRONMENT', 'production');
 		break;
